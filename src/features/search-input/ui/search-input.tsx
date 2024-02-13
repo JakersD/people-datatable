51fa69c74fdfe33d90fc00b31b316bc651796e43
Jakers <ilyaC303.spawn@outlook.com>
2024-02-13 19:28:28 +0300

    Не стал костылить изменение каретки, просто поменял ей цвет

--- src/features/search-input/ui/search-input.tsx
+++ src/features/search-input/ui/search-input.tsx
@@ -10,21 +10,23 @@ export const SearchInput: React.FC = () => {
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
 
-  const handleChangeActive = () => setActive(!active);
+  const handleChangeFocus = () => setActive(!active);
 
   return (
-    <div className={styles.inputWrapper}>
-      <img src={active ? searchActiveImg : searchNotActiveImg} alt='Поиск' />
-      <input
-        type='text'
-        className={styles.input}
-        placeholder={t(core.search.placeholder)}
-        value={value}
-        onChange={handleInputChange}
-        onFocus={handleChangeActive}
-        onBlur={handleChangeActive}
-      />
-      <img src={searchDropdownImg} alt='Фильтр' />
-    </div>
+    <>
+      <div className={styles.inputWrapper}>
+        <img src={active ? searchActiveImg : searchNotActiveImg} alt='Поиск' />
+        <input
+          type='text'
+          className={styles.input}
+          placeholder={active ? '' : t(core.search.placeholder)}
+          value={value}
+          onChange={handleInputChange}
+          onFocus={handleChangeFocus}
+          onBlur={handleChangeFocus}
+        />
+        <img src={searchDropdownImg} alt='Фильтр' />
+      </div>
+    </>
   );
 };
